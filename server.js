e182a7a3908446258daa35cc8d1e90dfdee0acb3
Taiyo Kanamori <Tahiyau000@gmail.com>
2024-08-22 09:39:48 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -6,8 +6,31 @@ import { serveDir } from 'http/file_server.ts'
 /**
  * APIリクエストを処理する
  */
-serve((req) => {
+const kv = new  DEnoKV();
+
+serve(async(req) => {
   // publicフォルダ内にあるファイルを返す
+  const url = new URL(req.url);
+
+  if (req.method === "POST" && url.pathname === "/record-time") {
+    const kv = await Deno.openKv();
+    console.log(kv);
+
+    const{type,time} = await req.json();
+    
+    if (type === "wake") {
+      await kv.set("wakeTime", time);
+    } else if (type === "sleep") {
+      await kv.set("sleepTime", time);
+    }
+
+    return new Response(JSON.stringify({ message: `${type}時間が記録されました。` }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+
   return serveDir(req, {
     fsRoot: 'public',
     urlRoot: '',
