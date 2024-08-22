45fbb4fad0a86e49af8ebde0d99d114409d9c936
Taiyo Kanamori <Tahiyau000@gmail.com>
2024-08-22 13:05:55 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ serve(async(req) => {
   // publicフォルダ内にあるファイルを返す
   const url = new URL(req.url);
   if (req.method === "POST" && url.pathname === "/record-time") {
-
     try {
       const { type, time } = await req.json();
       const timestamp = new Date().toISOString();
@@ -32,6 +31,27 @@ serve(async(req) => {
     }
   }
 
+  if (req.method === "POST" && url.pathname === "/delete-all-times") {
+    try {
+      // キーの取得と削除処理
+      const iterator = kv.list({ prefix: '' }); // プレフィックスが空で全てのキーを取得
+      for await (const { key } of iterator) {
+        await kv.delete(key);
+      }
+
+      return new Response(JSON.stringify({ message: "すべての時間が削除されました。" }), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    catch (error) {
+      console.error("Error processing request:", error.message);
+      return new Response(JSON.stringify({ message: "サーバーエラーが発生しました。" }), {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+  }
 
   return serveDir(req, {
     fsRoot: 'public',
